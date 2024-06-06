c87276054d521d1010238a3e1ad530c8c35acd63
wybrand <wybrandvdmeer@gmail.com>
2024-06-06 16:19:01 +0200

    init bug.

--- src/candle.h
+++ src/candle.h
@@ -5,7 +5,7 @@
 
 class candle {
 public:
-	int id;
+	int id=0;
 	long time, volume;
 	float open, close, low, high, adjustedClose;
 	candle(long time=0, float open=0.0, float close=0.0, float low=0.0, float high=0.0, float adjustedClose=0.0, long volume=0);
