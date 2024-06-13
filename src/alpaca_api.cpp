fbaa98a6335cba3852f8a0665d218bbd5cc810d5
wybrand <wybrandvdmeer@gmail.com>
2024-06-13 15:01:31 +0200

    Extended Alpaca buy call.

--- src/alpaca_api.cpp
+++ src/alpaca_api.cpp
@@ -10,18 +10,29 @@ bool alpaca_api::open_position(position &p) {
 	auto env = alpaca::Environment();
 	auto client = alpaca::Client(env);
 
+	alpaca::StopLossParams stop_loss_params;
+	stop_loss_params.stopPrice = std::to_string(p.loss_limit_price);
+
 	auto response = client.submitOrder(
   		p.ticker,
   		p.no_of_stocks,
   		alpaca::OrderSide::Buy,
   		alpaca::OrderType::Market,
-  		alpaca::OrderTimeInForce::Day
+  		alpaca::OrderTimeInForce::Day,
+		"",
+		"",
+		false,
+		"", 		// order id.
+		alpaca::OrderClass::Simple,
+		NULL,
+		&stop_loss_params
 	);
 
 	if (auto status = response.first; !status.ok()) {
 		log.log("Error calling API: %s", status.getMessage().c_str());
 		return false; 
 	}
+
 
 	return true;
 }
